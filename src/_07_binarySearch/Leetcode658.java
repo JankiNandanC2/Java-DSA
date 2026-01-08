f5bb3d4d395512ccc474877ae1d1b90293d4a4e8
JankiNandanC2 <jankinandan72@gmail.com>
2026-01-09 00:27:19 +0530

    Leetcode

---
+++ src/_07_binarySearch/Leetcode658.java
@@ -0,0 +1,12 @@
+package _07_binarySearch;
+
+public class Leetcode658 {
+    /*
+    658. Find K Closest Elements
+    Solved Medium
+
+    Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array.
+    The result should also be sorted in ascending order.
+
+    */
+}
---
+++ src/_07_binarySearch/Leetcode_1283.java
@@ -0,0 +1,7 @@
+package _07_binarySearch;
+
+public class Leetcode_1283 {
+    /*
+
+    */
+}
