ed888b97cea5131feec43cc1a0f40b0719846dfe
JankiNandanC2 <jankinandan72@gmail.com>
2026-04-06 21:29:42 +0530

    LinkedList

---
+++ src/_11_LinkedList/_01_Basics.java
@@ -0,0 +1,47 @@
+package _11_LinkedList;
+
+class Node {
+    int val ; // value
+    Node next ;
+    Node(int val){
+        this.val = val ;
+    }
+}
+public class _01_Basics {
+    static void main(String[] args) {
+        System.out.println();
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+// linking of nodes
+        a.next = b ; // linking =>  a -> b
+        b.next = c ; // linking =>  a -> b -> c
+        c.next = d ; // linking =>  a -> b -> c -> d
+        d.next = e ; // linking =>  a -> b -> c -> d -> e
+        // if we want to create a loop of linkedList we can put the address of first node in last node
+/*
+// to print the address of the nodes
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        System.out.println(a.next);
+        System.out.println(b.next);
+        System.out.println(c.next);
+        System.out.println(d.next);
+        System.out.println(e.next); //  last  node -->  null
+ */
+
+// print the value of nodess
+        System.out.println(a.val);
+        System.out.println(b.val);
+        System.out.println(c.val);
+        System.out.println(d.val);
+        System.out.println(e.val);
+    }
+}
